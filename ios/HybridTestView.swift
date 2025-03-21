e6f045a03a778bd0c91bb0f51f025991e7ed4d18
Ebisuzawa Kurumi <ebisuzawakurumi@proton.me>
2025-03-22 06:36:59 +0800

    feat: ios

--- ios/HybridTestView.swift
+++ ios/HybridTestView.swift
@@ -1,14 +1,18 @@
 import Foundation
 import UIKit
 
-class HybridTestView : HybridTestViewSpec {
-  // UIView
-  var view: UIView = UIView()
+class HybridTestView: HybridTestViewSpec {
+    
+    // UIView
+    var view: UIView = UIView()
 
-  // Props
-  var isRed: Bool = false {
-    didSet {
-      view.backgroundColor = isRed ? .red : .black
+    // Props
+    var isRed: Bool = false {
+        didSet {
+            view.backgroundColor = isRed ? .red : .black
+            onColorChange() // Call onColorChange when isRed is set
+        }
     }
-  }
+    
+    var onColorChange: () -> Void = {}
 }
